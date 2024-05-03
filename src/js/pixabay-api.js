633f9e2476e69a04e96582c36ff6e255dae07e14
mieguzki <152374584+mieguzki@users.noreply.github.com>
2024-05-03 12:07:22 +0300

    api2

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,21 +1,23 @@
-const BASE_URL = 'https://pixabay.com/api/';
-const KEY = '43233775-39d154ba994f40051e2e1ec64';
+function serviceGetImage(query){
 
-export function objectSearch(searchImage) {
-  const params = new URLSearchParams({
-    key: KEY,
-    q: searchImage,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-  });
-
-  return fetch(`${BASE_URL}/?${params}`).then(response => {
-    if (!response.ok) {
-      throw new Error(
-        'Sorry, there are no images matching your search query. Please try again!'
-      );
-    }
-    return response.json();
-  });
-}
+    const BASE_URL = 'https://pixabay.com/api/';
+    const API_KEY = '43682309-decfbd59f0a24849675c6bf75';
+  
+    const params = new URLSearchParams({
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    });
+  
+    return fetch(`${BASE_URL}?${params}`).then(resp => {
+      if (!resp.ok) {
+        throw new Error(resp.statusText);
+      }
+      return resp.json();
+    });
+  }
+  
+  
+  export { serviceGetImage };
